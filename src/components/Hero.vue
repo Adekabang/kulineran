c7764307c673da5d2d6db7f546f9cbf93e80ae4f
Adekabang <iban.ninja@gmail.com>
2020-09-26 00:47:22 +0700

    responsive hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="row mt-4">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
-          <p>Ayo segera pilih dan pesan makanan favorit anda</p>
-          <button class="btn btn-lg btn-success">
-            <b-icon-arrow-right></b-icon-arrow-right>Pesan
-          </button>
+  <div class="hero">
+    <!-- desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
+              <p>Ayo segera pilih dan pesan makanan favorit anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" alt="hero image" />
         </div>
       </div>
     </div>
-    <div class="col">
-      <img src="../assets/images/hero.png" width="100%" alt="hero image" />
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" width="100%" alt="hero image" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
+              <p>Ayo segera pilih dan pesan makanan favorit anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
